3a60f49392ae8b8b14b9882d91860687e0f833f0
Matthew Brooker <mattbr@hotmail.co.nz>
2018-04-30 20:35:49 +1200

    All investor types show in nav.

--- app/src/main/java/project/matthew/booster/UI/InvestorTypeFragment.java
+++ app/src/main/java/project/matthew/booster/UI/InvestorTypeFragment.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import project.matthew.booster.R;
@@ -26,16 +28,20 @@ public class InvestorTypeFragment extends Fragment implements InvestorTypeInterf
     @BindView(R.id.investor_type_pie_graph)
     ImageView investorTypePieGraphView;
 
+    @BindView(R.id.investor_type_frag_title)
+    TextView investorTypeFragTitle;
+
     @BindView(R.id.investor_type_description)
     TextView investorTypeDescription;
-
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.investor_type_fragment, container, false);
-        ButterKnife.bind(rootView);
+        ButterKnife.bind(this, rootView);
         mInvestorType = getTag();
+
+        investorTypeFragTitle.setText(mInvestorType);
 
         setPieGraphAndText();
 
@@ -46,19 +52,28 @@ public class InvestorTypeFragment extends Fragment implements InvestorTypeInterf
     public void setPieGraphAndText() {
         switch(mInvestorType) {
             case "Defensive":
-                // TODO: Assign appropriately.
-               // investorTypePieGraphView.setImageResource(R.drawable.booster_logo);
-               // investorTypeDescription.setText();
+                investorTypePieGraphView.setImageResource(R.drawable.capital_guaranteed_fund_pie_graph);
+                investorTypeDescription.setText(getResources().getString(R.string.capital_guarantee_fund));
                 break;
             case "Conservative":
+                investorTypePieGraphView.setImageResource(R.drawable.default_saver_fund_pie_graph);
+                investorTypeDescription.setText(getResources().getString(R.string.default_saver_fund));
                 break;
             case "Balanced":
+                investorTypePieGraphView.setImageResource(R.drawable.balanced_fund_pie_graph);
+                investorTypeDescription.setText(getResources().getString(R.string.balanced_fund));
                 break;
             case "Balanced Growth":
+                investorTypePieGraphView.setImageResource(R.drawable.balanced_growth_fund_pie_graph);
+                investorTypeDescription.setText(getResources().getString(R.string.balanced_growth_fund));
                 break;
             case "Growth":
+                investorTypePieGraphView.setImageResource(R.drawable.high_growth_fund_pie_graph);
+                investorTypeDescription.setText(getResources().getString(R.string.high_growth_fund));
                 break;
             case "Aggressive Growth":
+                investorTypePieGraphView.setImageResource(R.drawable.high_growth_fund_pie_graph);
+                investorTypeDescription.setText(getResources().getString(R.string.high_growth_fund));
                 break;
         }
     }
