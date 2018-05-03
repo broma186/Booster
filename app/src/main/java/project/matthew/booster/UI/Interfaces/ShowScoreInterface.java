3664aded6e5d4cef19a9e18ba0a2fa362742d948
Matthew Brooker <mattbr@hotmail.co.nz>
2018-05-03 22:53:52 +1200

    Score passed through to result fragment.

---
+++ app/src/main/java/project/matthew/booster/UI/Interfaces/ShowScoreInterface.java
@@ -0,0 +1,9 @@
+package project.matthew.booster.UI.Interfaces;
+
+/**
+ * Created by Matthew on 3/05/2018.
+ */
+
+public interface ShowScoreInterface {
+    public void showScore();
+}
--- app/src/main/java/project/matthew/booster/UI/ResultFragment.java
+++ app/src/main/java/project/matthew/booster/UI/ResultFragment.java
@@ -1,5 +1,6 @@
 package project.matthew.booster.UI;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,12 +13,13 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import project.matthew.booster.R;
+import project.matthew.booster.UI.Interfaces.ShowScoreInterface;
 
 /**
  * Created by Matthew on 3/05/2018.
  */
 
-public class ResultFragment extends Fragment{
+public class ResultFragment extends Fragment implements ShowScoreInterface {
 
     private View rootView;
     private MainActivity mainActivity;
@@ -25,6 +27,10 @@ public class ResultFragment extends Fragment{
     @BindView(R.id.total_score)
     TextView totalScore;
 
+    @BindView(R.id.investment_result)
+    TextView investmentResult;
+
+    private int score;
 
 
     @Nullable
@@ -36,12 +42,28 @@ public class ResultFragment extends Fragment{
         mainActivity = ((MainActivity) getActivity());
 
         totalScore.setText(mainActivity.getScore());
+        score = mainActivity.getScore();
 
-        
+
 
         return rootView;
     }
 
-
-
+    @Override
+    public void showScore() {
+        Resources res = getActivity().getResources();
+        if (score >= 5 && score <= 12) {
+            investmentResult.setText(res.getString(R.string.result_defensive));
+        } else if (score >= 13 && score <= 20) {
+            investmentResult.setText(res.getString(R.string.result_conservative));
+        } else if (score >= 21 && score <= 29) {
+            investmentResult.setText(res.getString(R.string.result_balanced));
+        } else if (score >= 30 && score <= 37) {
+            investmentResult.setText(res.getString(R.string.result_balanced_growth));
+        } else if (score >= 38 && score <= 44) {
+            investmentResult.setText(res.getString(R.string.result_growth));
+        } else if (score >= 45 && score <= 50) {
+            investmentResult.setText(res.getString(R.string.result_aggressive_growth));
+        }
+    }
 }
